6750a7343e483fd24c9131475ec4b103e96699ea
László Vaskó <1771332+vlaci@users.noreply.github.com>
2023-05-18 23:38:13 +0200

    test(python): test error handling API

--- tests/test_python.py
+++ tests/test_python.py
@@ -12,3 +12,21 @@ def test_roundtrip(lorem):
     comp = c.compress(lorem)
 
     assert lzallright.LZOCompressor.decompress(comp) == lorem
+
+
+def test_decompress_partial(lorem):
+    c = lzallright.LZOCompressor()
+    comp = c.compress(lorem)
+
+    with pytest.raises(lzallright.InputNotConsumed) as exc:
+        lzallright.LZOCompressor.decompress(comp + b"foobar")
+
+    assert exc.value.args == (lzallright.EResult.InputNotConsumed, lorem)
+    assert issubclass(exc.type, lzallright.LZOError)
+
+
+def test_decompress_error(lorem):
+    with pytest.raises(lzallright.LZOError) as exc:
+        lzallright.LZOCompressor.decompress(lorem)
+
+    assert exc.value.args == (lzallright.EResult.LookbehindOverrun,)
